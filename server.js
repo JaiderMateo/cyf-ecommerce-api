675c5f4e0763858f0ea478a713720fbff4ecd084
Jaider Mateo Soto <jaidermateo1010@gmail.com>
2021-06-25 18:47:49 +0200

    homework done

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const pool = new Pool ({
     database: 'cyf_ecommerce',
     password: '1234',
     port: 5432
-})
+});
 
 app.listen(3000, ()=>{
     console.log("Server is listening on port 3000. Ready to accept requests!");
-})
+});
 
 app.get("/customers", (request, response)=>{
     pool.query('select * from customers', (error, result)=>{
@@ -23,5 +23,11 @@ app.get("/customers", (request, response)=>{
 app.get("/suppliers", (request, response)=>{
     pool.query('select * from suppliers', (error, result)=>{
         response.json(result.rows);
-    })
+    });
+});
+
+app.get("/products", (request, response)=>{
+    pool.query('select p.product_name , s.supplier_name from suppliers s inner join products p on p.supplier_id = s.id', (error, result)=>{
+        response.json(result.rows);
+    });
 });
